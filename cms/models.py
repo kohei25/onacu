10f8b2fbe59ba85bf653cb4ff1b20ffc653b0c2d
kohei25 <nicodoteng@gmail.com>
2020-05-15 08:06:16 +0900

    add status column @ event model

--- cms/models.py
+++ cms/models.py
@@ -124,6 +124,9 @@ class Event(models.Model):
   purchaced_ticket = models.IntegerField(default=0)
   # 120枚は,30s/回で60分の計算
   total_ticket = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(120)])
+  # eventの状態を表す
+  # 0: イベント前，1: イベント前, 2: イベント中，3:イベント後
+  status = models.IntegerField(default=0)
 
   def __str__(self):
     return self.name
