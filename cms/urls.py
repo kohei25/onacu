da0ceaba2381f7100baf6f3c0dcf84222c6251f6
kohei25 <nicodoteng@gmail.com>
2020-05-19 06:10:43 +0900

    config top1 page

--- cms/urls.py
+++ cms/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'cms'
 urlpatterns = [
     path('', views.TopView.as_view(), name='top'),
+    path('top/', views.Top1View.as_view(), name='top1'),
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
     path('signup/', views.UserCreate.as_view(), name='signup'),
