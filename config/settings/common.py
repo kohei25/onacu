ca370464abe70836badcb55b0dea5df788feb5f4
kohei25 <nicodoteng@gmail.com>
2020-06-02 02:27:40 +0900

    update config s3

--- config/settings/common.py
+++ config/settings/common.py
@@ -106,18 +106,3 @@ AUTH_USER_MODEL = 'cms.User'
 LOGIN_URL = 'cms:login'
 LOGIN_REDIRECT_URL = 'cms:top'
 LOGOUT_REDIRECT_URL = 'cms:top'
-
-# S3
-AWS_ACCESS_KEY_ID = os.environ["ACCESS_KEY_ID"]
-AWS_SECRET_ACCESS_KEY = os.environ["SECRET_ACCESS_KEY"]
-AWS_STORAGE_BUCKET_NAME = os.environ["BUCKET_NAME"]
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',  # 1日はそのキャッシュを使う
-}
-
-AWS_LOCATION = 'static'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-
-DEFAULT_FILE_STORAGE = 'config.storage_backends.MediaStorage'
--- config/settings/production.py
+++ config/settings/production.py
@@ -21,3 +21,17 @@ DATABASES = {
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'www', 'static')
 
+# S3
+AWS_ACCESS_KEY_ID = os.environ["ACCESS_KEY_ID"]
+AWS_SECRET_ACCESS_KEY = os.environ["SECRET_ACCESS_KEY"]
+AWS_STORAGE_BUCKET_NAME = os.environ["BUCKET_NAME"]
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',  # 1日はそのキャッシュを使う
+}
+
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'config.storage_backends.MediaStorage'
