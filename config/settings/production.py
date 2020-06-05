1c4bf63e72b558221b98ee0912be478817a11437
kohei25 <nicodoteng@gmail.com>
2020-06-06 01:42:03 +0900

    config 'ses', 'security', 'sass'

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,11 +1,16 @@
 from .common import *
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
   'onacuproduction-env.eba-izd3p7h2.ap-northeast-1.elasticbeanstalk.com',
   'onacu.org',
 ]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
@@ -31,18 +36,26 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',  # 1日はそのキャッシュを使う
 }
 
-# AWS_LOCATION = 'static'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-DEFAULT_FILE_STORAGE = 'config.storage_backends.MediaStorage'
+SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+SASS_PROCESSOR_AUTO_INCLUDE = False
+STATICFILES_DIR = (
+  os.path.join(BASE_DIR, 'static')
+)
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+]
+SASS_PRECISION = 5
+SASS_OUTPUT_STYLE = 'compressed'
 
 # Email
 EMAIL_BACKEND = 'django_ses.SESBackend'
-# us-east-1 以外のAWSリージョンを使用する場合はこれも必要↓
-# AWS_SES_REGION_NAME = 'us-west-2'
-# AWS_SES_REGION_ENDPOINT = 'email.us-west-2.amazonaws.com'
-
-# DEFAULT_FROM_EMAIL: サイト管理者からの自動送信メールに使用するデフォルトの Email アドレス
-# SERVER_EMAIL: ADMINS や MANAGERS に送信されるエラーメッセージの送信元 Email アドレス
 DEFAULT_FROM_EMAIL = SERVER_EMAIL = 'no-reply <noreply@onacu.org>'
+
+DEFAULT_FILE_STORAGE = 'config.storage_backends.MediaStorage'
+AWS_DEFAULT_ACL = None
