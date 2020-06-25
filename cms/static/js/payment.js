b339d6b36fb57936e9395acebd1c6bbc812468d3
kohei25 <nicodoteng@gmail.com>
2020-06-25 23:58:47 +0900

    add payment view

---
+++ cms/static/js/payment.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+
+  console.log("Sanity check!");
+
+  // new
+  // Get Stripe publishable key
+  fetch("/config/")
+    .then((result) => { return result.json(); })
+    .then((data) => {
+      // Initialize Stripe.js
+      const stripe = Stripe(data.publicKey);
+    });
+});
